58cc52b6d9822ff79aa7194c7ed97ec46031ddd0
Arnabshrivastav <43786210+Arnabshrivastav@users.noreply.github.com>
2018-10-13 23:53:03 +0530

    update helloworld

--- GettingStarted/helloworld.cpp
+++ GettingStarted/helloworld.cpp
@@ -1,7 +1,8 @@
+/*This is a comment telling that the program is to print Hello World! as output*/
 #include<iostream>
 using namespace std;
 int main{
 
-cout<<"Hello World"<<endl;
+cout<<"Hello World!"<<endl;
 return 0;
 }
